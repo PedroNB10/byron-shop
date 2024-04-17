9314f097e74dd798595ce8820d003f76d39ec987
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-04-17 16:59:58 -0300

    feat: adicionar novas rotas de busca de usuario por id e login. Além disso adicionar a middleware de login.

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,6 +1,42 @@
 import { PrismaClient } from "@prisma/client";
+import { gerarToken } from "../utils/jwt.js";
 
 const prisma = new PrismaClient();
+
+export const getUsuarios = async (req, res) => {
+  const usuarios = await prisma.usuario.findMany({
+    include: {
+      carrinhos: true,
+    },
+  });
+
+  res.json({
+    data: usuarios,
+  });
+};
+
+export const getUsuarioPorId = async (req, res) => {
+  // console.log(req.params.usuarioId);
+  const usuario = await prisma.usuario.findFirst({
+    where: {
+      id: parseInt(req.params.usuarioId),
+    },
+    include: {
+      carrinhos: true,
+    },
+  });
+
+  if (!usuario) {
+    res.status(404).json({
+      msg: "Usuário não encontrado",
+    });
+    return;
+  }
+
+  res.json({
+    data: usuario,
+  });
+};
 
 export const criarUsuario = async (req, res) => {
   const data = req.body.data;
@@ -41,8 +77,11 @@ export const criarUsuario = async (req, res) => {
       },
     });
 
+    const token = gerarToken(usuario);
+
     res.json({
       data: usuario,
+      token: token,
       msg: "Categoria criada com sucesso",
     });
   } catch {
@@ -50,4 +89,42 @@ export const criarUsuario = async (req, res) => {
       msg: "Erro ao criar usuário",
     });
   }
+};
+
+export const login = async (req, res) => {
+  const data = req.body.data;
+
+  if (!data.email || !data.senha) {
+    res.status(400).json({
+      msg: "Dados obrigatórios não foram preenchidos",
+    });
+  }
+
+  const usuario = await prisma.usuario.findFirst({
+    where: {
+      AND: [
+        {
+          email: data.email,
+        },
+        {
+          senha: data.senha,
+        },
+      ],
+    },
+  });
+
+  if (!usuario) {
+    res.status(401).json({
+      msg: "Login ou a senha Estão incorretos, rota não autorizada",
+    });
+    return;
+  }
+
+  const token = gerarToken(usuario);
+
+  res.json({
+    data: usuario,
+    token: token,
+    msg: "Login realizado com sucesso",
+  });
 };
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import * as usuarioController from "../controllers/usuario.controller.js";
+import autorizarUsuario from "../middlewares/auth.middlewares.js";
 
 const router = express.Router();
 
 router.post("/", usuarioController.criarUsuario);
+router.post("/login", usuarioController.login);
+// para usar a middleware de autorização, basta passar a função como segundo argumento
+// router.get("/", autorizarUsuario, usuarioController.getUsuarios);
+router.get("/", usuarioController.getUsuarios);
+router.get("/:usuarioId", usuarioController.getUsuarioPorId);
 export default router;
