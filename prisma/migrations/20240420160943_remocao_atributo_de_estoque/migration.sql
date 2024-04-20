8a27029021b073ee637847d71d412d3c7c5beafa
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-04-20 13:10:06 -0300

    feat: atualização da migration para a tabela de estoque.

---
+++ prisma/migrations/20240420160943_remocao_atributo_de_estoque/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quantidade_pedida` on the `estoque` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `estoque` DROP COLUMN `quantidade_pedida`;
