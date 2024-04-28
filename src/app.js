2b6a33b243f1d01db902225aee5eeb6ed010d795
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-04-28 16:58:18 -0300

    docs: adicionar a configuração do swagger.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "./swagger.json" assert { type: "json" };
+
 import exampleRouter from "./routes/example.routes.js";
 import produtosRouter from "./routes/produtos.routes.js";
 import usuarioRouter from "./routes/usuario.routes.js";
@@ -7,13 +10,15 @@ import carrinhoRouter from "./routes/carrinho.routes.js";
 
 const app = express();
 const port = process.env.APP_PORT ?? 8080;
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? "localhost";
 
 // app.use(express.urlencoded({extended: true})); // middleware para trabalhar com formulários
 app.use(express.json());
 // express.json() is a middleware function provided by the Express.js framework. It's used to parse incoming request bodies with JSON payloads.
 
 // When you send data to your server using HTTP POST or PUT requests with a JSON payload in the body (common in APIs), express.json() parses that JSON data and makes it available in req.body object of your Express route handlers.
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
